4f324925ca5de522254fea1e2c36a02650b1a6f2
Ho, Ming-Hsin <charlie.ho@itri.org.tw>
2024-04-22 16:49:43 +0800

    gjk-distance: keep debug data

--- gjk-distance/main.cpp
+++ gjk-distance/main.cpp
@@ -15,6 +15,23 @@ int main(int argc, char *argv[])
     std::vector<cv::Point2f> s1 = GenerateHull(std::atof(argv[1]), std::atof(argv[2]));
     std::vector<cv::Point2f> s2 = GenerateHull(std::atof(argv[3]), std::atof(argv[4]));
 
+    // std::vector<cv::Point2f> s1;
+    // cv::Point2f p; 
+    // p.x = -4.33304; p.y = 6.12644; s1.push_back(p); 
+    // p.x = -5.13327; p.y = 9.21342; s1.push_back(p); 
+    // p.x = -12.3496; p.y = 6.6309;  s1.push_back(p); 
+    // p.x = -13.9803; p.y = 5.25541; s1.push_back(p); 
+    // p.x = -13.5747; p.y = 1.5156;  s1.push_back(p); 
+    // p.x = -9.76489; p.y = 1.64857; s1.push_back(p); 
+    // p.x = -7.48916; p.y = 2.36117; s1.push_back(p); 
+
+    // std::vector<cv::Point2f> s2;
+    // p.x = 17.7267; p.y = -5.72086; s2.push_back(p); 
+    // p.x = 10.2123; p.y = -1.58326; s2.push_back(p); 
+    // p.x = 5.71667; p.y = -2.27288; s2.push_back(p); 
+    // p.x = 12.2122; p.y = -7.12906; s2.push_back(p); 
+
+
     printf("s1 num: %i\n", s1.size());
     for (int i = 0; i < s1.size(); i++)
     {
