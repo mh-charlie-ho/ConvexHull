3a20471b15c37295acaea1d44a212991bb33f4f7
Ho, Ming-Hsin <charlie.ho@itri.org.tw>
2024-04-22 10:36:54 +0800

    gjk-overlapping:  debug print

--- gjk-overlapping/gjk.cpp
+++ gjk-overlapping/gjk.cpp
@@ -1,29 +1,33 @@
 #include "gjk.hpp"
+#include <iostream>
+
+using namespace std;
 
 static Eigen::Vector2f prod(Eigen::Vector2f a, Eigen::Vector2f b)
 {
     return (1 - (a.dot(b) / (a.norm() * b.norm()))) * b;
 }
 
-GJK::GJK(ConvexHull s1, ConvexHull s2)
+GJK::GJK(const ConvexHull &s1, const ConvexHull &s2)
     : mS1(s1), mS2(s2), mOrigin(0, 0)
 {
 }
 
-GJK::GJK(std::vector<cv::Point2f> s1, std::vector<cv::Point2f> s2)
+GJK::GJK(const std::vector<cv::Point2f> &s1, const std::vector<cv::Point2f> &s2)
     : mOrigin(0, 0)
 {
     mS1 = ConvertFormat(s1);
     mS2 = ConvertFormat(s2);
 }
 
-ConvexHull GJK::ConvertFormat(std::vector<cv::Point2f> points)
+ConvexHull GJK::ConvertFormat(
+    const std::vector<cv::Point2f> &points)
 {
     ConvexHull outputConvexHull;
 
     float xSum = 0;
     float ySum = 0;
-    for (int i = 0; i <= points.size(); i++)
+    for (int i = 0; i < points.size(); i++)
     {
         xSum += points[i].x;
         ySum += points[i].y;
@@ -35,24 +39,44 @@ ConvexHull GJK::ConvertFormat(std::vector<cv::Point2f> points)
     return outputConvexHull;
 }
 
-Eigen::Vector2f GJK::SupportFunciton(ConvexHull s, Eigen::Vector2f d)
+Eigen::Vector2f GJK::SupportFunciton(
+    const ConvexHull &s,
+    const Eigen::Vector2f &d)
 {
+    cout << "SupportFunciton" << endl;
     float tempdot = -1; // 單位向量投影最小值為-1
     int supportPtId = 0;
+    cout << s.vertex.size() << endl;
     for (int i = 0; i < s.vertex.size(); i++)
     {
-        float dot = d[0] * s.vertex[i][0] + d[1] * s.vertex[i][1];
+        Eigen::Vector2f v;
+        cout << s.vertex[i][0] << s.vertex[i][1] << endl;
+        v << s.vertex[i][0], s.vertex[i][1];
+        // cout << v << "\n" << endl;
+        v.normalize();
+        // cout << v << "\n" << endl;
+
+
+
+        float dot = d[0] * v[0] + d[1] * v[1];
+        cout << v[0] << v[1] << endl;
+        cout << dot << endl;
         if (tempdot < dot)
         {
             tempdot = dot;
             supportPtId = i;
         }
+
+        cout << "supportPtId" << supportPtId << endl;
     }
+    cout << "---------------------------" << endl;
     return s.vertex[supportPtId];
 }
 
-Eigen::Vector2f GJK::Support(Eigen::Vector2f d)
+Eigen::Vector2f GJK::Support(const Eigen::Vector2f &d)
 {
+    cout << d << "\n" << endl;
+    cout << mS2.vertex.size() << endl;
     return (SupportFunciton(mS2, d) - SupportFunciton(mS1, -d));
 }
 
@@ -112,26 +136,41 @@ bool GJK::TriangleCase(ConvexHull &simplex, Eigen::Vector2f &d)
     return true; // 在線上或是中間區域
 }
 
+void GJK::PrintSimplex(const ConvexHull &simplex)
+{
+    for (int i = 0; i < simplex.vertex.size(); i++)
+    {
+        printf(
+            "simplex vertex (x, y): %f, %f\n",
+            simplex.vertex[i][0], simplex.vertex[i][1]);
+    }
+}
+
 bool GJK::CollisionCheck()
 {
     Eigen::Vector2f d(mS2.center - mS1.center);
     d.normalize();
-    // So the vector d is built.
 
     ConvexHull simplex;
-    // 一開始就選到原點直接return
     simplex.vertex.push_back(Support(d));
+
+    // 一開始就選到原點直接return
     if (simplex.vertex[0] == mOrigin)
     {
         return true;
     }
-
     d << mOrigin - simplex.vertex[0];
 
     while (true)
     {
         d.normalize();
+        cout << d << "\n"
+             << endl;
         Eigen::Vector2f p(Support(d)); // 一條線求第三個點/一個點求第二個點
+        
+        cout << "come here" << endl;
+        cout << Support(d) << "\n"
+             << endl;
         if (p == mOrigin)
         {
             return true; // 如果是原點就代表有重疊
